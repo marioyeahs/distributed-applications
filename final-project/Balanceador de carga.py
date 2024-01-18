36d8cd132ef3b0088e8257b9aadee53190b58709
marioyeahs <marioyeahs@gmail.com>
2024-01-17 20:48:05 -0600

    add: load balancer

---
+++ final-project/Balanceador de carga.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import requests
+import threading
+
+app = Flask(__name__)
+
+def redirect_requests():
+    while True:
+        response = requests.get('http://127.0.0.1:5000/client')
+        print(response.json())
+
+@app.route('/redirect', methods=['GET'])
+def redirect():
+    threading.Thread(target=redirect_requests).start()
+    return jsonify({'message': 'Redirección iniciada'})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=12346)
